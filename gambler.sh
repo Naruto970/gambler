9d32c8f76d8933d493b458d5814f2642dfad6070
mugiwarapirates <hemilturakhia@gmail.com>
2019-11-14 09:42:20 +0530

    user will win or loss and stake will be changed simultaneously

--- gambler.sh
+++ gambler.sh
@@ -6,18 +6,25 @@ read -p "no. of games that you want to play: " games
 
 bets=0
 stake=0
+wins=0
+loss=0
 
 for (( i=0 ; i<$days ; i++ ))
 {
       stake=$(( $stake +100 ))
    for (( j=0 ; j<$games ; j++ ))
 	{    
-           ((bets++))
+              value=$((RANDOM%2))
+             ((bets++))
    	if [ $stake -eq 0]
         then
-                  break            
+                  break
+        elif [ $value -eq 0  ] 
+        then      
+                 ((wins++))             
                  ((stake++))
         else
+                 ((loss++)) 
                  ((stake--))
         fi
     }
@@ -25,3 +32,5 @@ for (( i=0 ; i<$days ; i++ ))
 
 echo "the bets you made are: " $bets
 echo "the total no. of stake is: " $stake 
+echo "the no. of wins are: " $wins
+echo "the no. of losses are: " $loss
